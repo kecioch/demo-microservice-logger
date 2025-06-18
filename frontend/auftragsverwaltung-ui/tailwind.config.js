64a7894170e6dfc3289a1202d9d4eb5b5701ac96
Kevin Cioch <kevin.cioch@gmail.com>
2025-06-18 17:23:36 +0200

    Added HeroUI

--- frontend/auftragsverwaltung-ui/tailwind.config.js
+++ frontend/auftragsverwaltung-ui/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const {heroui} = require("@heroui/react");
+
 module.exports = {
   content: [
      "./src/**/*.{js,jsx,ts,tsx}",
+     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [heroui()],
 }
 
