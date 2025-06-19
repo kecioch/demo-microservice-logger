110a171316dddabf844e257bffc725325cd31933
Kevin Cioch <kevin.cioch@gmail.com>
2025-06-19 13:27:08 +0200

    Implemented prisma and basic persistence in backend

--- backend/persistence/src/index.ts
+++ backend/persistence/src/index.ts
@@ -1,5 +1,11 @@
 import app from './app';
 import config from './config/config';
+import express from "express";
+import { PrismaClient } from './generated/prisma';
+
+export const prisma = new PrismaClient();
+
+app.use(express.json());
 
 app.listen(config.port, () => {
   console.log(`Persistence-Server running on port ${config.port}`);
--- backend/persistence/src/routes/persistenceRoutes.ts
+++ backend/persistence/src/routes/persistenceRoutes.ts
@@ -1,10 +1,44 @@
 import { Router } from 'express';
+import { prisma } from '..';
+import { OrderState } from '../generated/prisma';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.status(200);
-    res.send("Hello from GET Persistence !!!");
+router.get('/', async (req, res) => {
+  try {
+    const orders = await prisma.order.findMany();
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error('Error creating order:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { title, description, state } = req.body;
+    console.log('POST', title, description, state);
+
+    // Validation
+    if (!Object.values(OrderState).includes(state)) {
+      res.status(400).json({ message: 'Invalid order state' });
+      return;
+    }
+
+    // Create Order
+    const order = await prisma.order.create({
+      data: {
+        title,
+        description,
+        state: state as OrderState,
+      },
+    });
+
+    res.status(201).json(order);
+  } catch (error) {
+    console.error('Error creating order:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 export default router;
